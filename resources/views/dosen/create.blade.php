edf95fc6e12b726f409886f66c2b1f9fdb9d2abc
danielinzaghi123 <132965130+danielinzaghi123@users.noreply.github.com>
2024-08-13 22:58:12 +0700

    merevisi

--- resources/views/dosen/create.blade.php
+++ resources/views/dosen/create.blade.php
@@ -32,7 +32,7 @@
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                                 required>
                                 <option value="">Select User</option>
-                                @foreach ($mahasiswa as $user)
+                                @foreach ($users as $user)
                                     <option value="{{ $user->id }}" data-name="{{ $user->name }}">{{ $user->name }}</option>
                                 @endforeach
                             </select>
