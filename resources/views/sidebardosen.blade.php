cffdac49bb352db542f26e2727aa6f86d0852445
danielinzaghi123 <132965130+danielinzaghi123@users.noreply.github.com>
2024-08-15 09:13:32 +0700

    akhirnyajadi

--- resources/views/sidebardosen.blade.php
+++ resources/views/sidebardosen.blade.php
@@ -30,9 +30,7 @@
                 </a>
             </li>
 
-
-            {{-- @if (isset($dosen) && $dosen->kelas_id) --}}
-            @if (isset($dosen) && $dosen->kelas_id)
+            @if (Route::currentRouteName() !== 'dosen.index')
                 <li>
                     <a href="{{ route('dosen.show2') }}"
                         class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
