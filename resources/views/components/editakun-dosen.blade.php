f2a8e9c2791c84bda6c3fb89ca49e04044c0f8c0
dzaki201 <mfikri.magangjogja@gmail.com>
2024-08-16 07:27:54 +0700

    revisi dikit edit akun dosen

--- resources/views/components/editakun-dosen.blade.php
+++ resources/views/components/editakun-dosen.blade.php
@@ -30,7 +30,7 @@
                         @method('PUT')
                         <div class="mb-4">
                             <label for="name"
-                                class="block text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                                class="block text-sm font-medium text-gray-900 dark:text-white">Username</label>
                             <input type="text" name="name" id="name" value="{{ $d->user->name }}"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                                 required>
@@ -47,16 +47,14 @@
                                 class="block text-sm font-medium text-gray-900 dark:text-white">Password</label>
                             <input type="password" name="password" id="password"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white">
+                            <p class="mt-1 text-sm text-gray-500 dark:text-gray-400">Kosongkan jika tidak ingin mengganti password.</p>
                         </div>
                         <!-- Modal footer -->
                         <div
                             class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
                             <button type="submit"
-                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Save
-                                changes</button>
-                            <button type="button"
-                                class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
-                                data-modal-hide="updateAkunDosen{{ $d->id_user }}">Cancel</button>
+                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Simpan
+                            </button>
                         </div>
                     </form>
                 </div>
@@ -64,8 +62,3 @@
         </div>
     </div>
 </div>
-<script>
-    document.addEventListener("DOMContentLoaded", function(event) {
-        document.getElementById('defaultModalButton').click();
-    });
-</script>
