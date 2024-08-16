094e5430611710e9b4d8714b5d4a6dbee44a7725
Puputera <127200098+Puputera@users.noreply.github.com>
2024-08-16 10:02:58 +0700

    revisi edit data dan akun mahasiswa

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Mahasiswa;
 use App\Models\RequestEdit;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class MahasiswaController extends Controller
 {
@@ -24,6 +26,29 @@ class MahasiswaController extends Controller
 
     public function updateMahasiswa(Request $request)
     {
+        // dd($request->all());
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,email,' . $request->id_user,
+            'password' => 'nullable|string|min:8',
+            'nama' => 'required',
+            'string',
+            'nim' => 'required',
+            'string',
+            'tempat_lahir' => 'required',
+            'string',
+            'tanggal_lahir' => 'required',
+            'date'
+        ]);
+
+        $user = User::findOrFail($request->id_user);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        if ($request->password) {
+            $user->password = Hash::make($request->password);
+        }
+        $user->save();
+
         $mahasiswa = Mahasiswa::where('id', $request->id)->first();
         $mahasiswa->update([
             'nama' => $request->nama,
