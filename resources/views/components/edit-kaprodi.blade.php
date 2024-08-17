e7610fa011e5d41529a0a68aa170a2004089fec2
Puputera <127200098+Puputera@users.noreply.github.com>
2024-08-17 07:31:06 +0700

    revisi edit kaprodi

--- resources/views/components/edit-kaprodi.blade.php
+++ resources/views/components/edit-kaprodi.blade.php
@@ -1,33 +1,24 @@
-<div id="updateProductModal{{ $kaprodi->id }}" tabindex="-1"
-    aria-hidden="true"
+<div id="updateProductModal{{ $kaprodi->id }}" tabindex="-1" aria-hidden="true"
     class="hidden overflow-y-auto overflow-x-hidden fixed inset-0 z-50 bg-black bg-opacity-50 justify-center items-center">
     <div class="relative w-full max-w-2xl max-h-full">
-        <form action="{{ route('kaprodi.update') }}" method="POST"
-            enctype="multipart/form-data">
+        <form action="{{ route('kaprodi.update') }}" method="POST" enctype="multipart/form-data">
             @csrf
             @method('POST')
-            <input type="hidden" name="id"
-                value="{{ $kaprodi->id }}">
-            <input type="hidden" name="id_user"
-                value="{{ $kaprodi->id_user }}">
+            <input type="hidden" name="id" value="{{ $kaprodi->id }}">
+            <input type="hidden" name="id_user" value="{{ $kaprodi->id_user }}">
             <!-- Modal content -->
-            <div
-                class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
                 <!-- Modal header -->
-                <div
-                    class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
-                    <h3
-                        class="text-xl font-semibold text-gray-900 dark:text-white">
-                        Edit Data Dosen
+                <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                    <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                        Edit Data
                     </h3>
                     <button type="button"
                         class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
                         data-modal-hide="updateProductModal{{ $kaprodi->id }}">
-                        <svg aria-hidden="true" class="w-5 h-5"
-                            fill="currentColor" viewbox="0 0 20 20"
+                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewbox="0 0 20 20"
                             xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                                clip-rule="evenodd"
+                            <path fill-rule="evenodd" clip-rule="evenodd"
                                 d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 011.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" />
                         </svg>
                         <span class="sr-only">Close modal</span>
@@ -35,23 +26,46 @@
                 </div>
 
                 <!-- Modal body -->
-                <div class="p-6 space-y-6">
-                    <div class="grid gap-6 mb-6 md:grid-cols-2">
-                        <div class="sm:col-span-2">
+
+                <div class="grid grid-cols-1 md:grid-cols-2 gap-6 p-6 ">
+                    {{-- kolom kiri --}}
+                    <div>
+                        <div class="w-full">
+                            <label for="name"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
+                            <input type="text" name="name" id="name" value="{{ $kaprodi->user->name }}"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="Email" required="">
+                        </div>
+                        <div class="w-full">
+                            <label for="email"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                            <input type="email" name="email" id="email" value="{{ $kaprodi->user->email }}"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="Email" required="">
+                        </div>
+                        <div class="w-full">
+                            <label for="password"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                            <input type="password" name="password" id="password"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="Password (Kosongkan jika tidak ingin mengubah)">
+                        </div>
+                    </div>
+
+                    {{-- Kolom kanan --}}
+                    <div>
+                        <div class="w-full">
                             <label for="nama"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama</label>
-                            <input type="text" name="nama"
-                                id="nama"
-                                value="{{ $kaprodi->nama }}"
+                            <input type="text" name="nama" id="nama" value="{{ $kaprodi->nama }}"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="Nama Dosen" required="">
                         </div>
                         <div class="w-full">
                             <label for="nip"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">NIP</label>
-                            <input type="number" name="nip"
-                                id="nip"
-                                value="{{ $kaprodi->nip }}"
+                            <input type="number" name="nip" id="nip" value="{{ $kaprodi->nip }}"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="NIP" required="">
                         </div>
@@ -59,43 +73,21 @@
                             <label for="kode_dosen"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kode
                                 Dosen</label>
-                            <input type="number" name="kode_dosen"
-                                id="kode_dosen"
-                                value="{{ $kaprodi->kode_dosen }}"
+                            <input type="number" name="kode_dosen" id="kode_dosen" value="{{ $kaprodi->kode_dosen }}"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="Kode Dosen" required="">
                         </div>
-                        <div class="w-full">
-                            <label for="name"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
-                            <input type="text" name="name"
-                                id="name"
-                                value="{{ $kaprodi->user->name }}"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                placeholder="Email" required="">
-                        </div>
-                        <div class="w-full">
-                            <label for="email"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                            <input type="email" name="email"
-                                id="email"
-                                value="{{ $kaprodi->user->email }}"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                placeholder="Email" required="">
-                        </div>
-                        <div class="w-full">
-                            <label for="password"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                            <input type="password" name="password"
-                                id="password"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                placeholder="Password (Kosongkan jika tidak ingin mengubah)">
-                        </div>
+                    </div>
+                </div>
+
+                <div class="p-6 space-y-6">
+                    <div class="grid gap-6 mb-6 md:grid-cols-2">
+
+
                     </div>
                 </div>
                 <!-- Modal footer -->
-                <div
-                    class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+                <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
                     <button type="submit"
                         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Simpan
                         Perubahan</button>
