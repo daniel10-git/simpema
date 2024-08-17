a4ddbd66e87b2d2b916214ee5a2e2d71c57a8623
Arisa Restu Ambarwati <arisarestuambarwati@gmail.com>
2024-08-17 15:01:35 +0700

    revisi nim agar saat diedit hanya bisa diinputkan dgn angka

--- resources/views/components/edit-mhs.blade.php
+++ resources/views/components/edit-mhs.blade.php
@@ -39,7 +39,7 @@
                         <div>
                             <label for="name"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama</label>
-                            <input type="text" name="nama" id="nama" value="{{ $m->nama }}"
+                            <input type="nama" name="nama" id="nama" value="{{ $m->nama }}"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                                 placeholder="" required="">
                         </div>
@@ -47,7 +47,7 @@
                             <label for="name"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tempat
                                 Lahir</label>
-                            <input type="text" name="tempat_lahir" id="nama" value="{{ $m->tempat_lahir }}"
+                            <input type="tempat_lahir" name="tempat_lahir" id="nama" value="{{ $m->tempat_lahir }}"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                                 placeholder="" required="">
                         </div>
@@ -70,6 +70,15 @@
         </div>
     </div>
 </div>
+
+<script>
+    document.querySelectorAll('input[type="text"]').forEach(input => {
+        input.addEventListener('input', function(e) {
+            this.value = this.value.replace(/\D/g, ''); // Hanya izinkan angka
+        });
+    });
+</script>
+
 <script>
     document.addEventListener("DOMContentLoaded", function(event) {
         document.getElementById('defaultModalButton').click();
